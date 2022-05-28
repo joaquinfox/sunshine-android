2f1c114567eac9e6813c899a4c8140454264c816
jimmy <joaquin.fox@gmail.com>
2022-05-28 13:05:13 -0700

    first commit

--- app/src/main/java/com/example/s0101wc/MainActivity.java
+++ app/src/main/java/com/example/s0101wc/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.s0101wc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView instanceTextView = (TextView) findViewById(R.id.tv_weather_data);
+        instanceTextView.append("\n\n But this text is appended from onCreate()");
+
     }
 }
